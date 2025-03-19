2d5174a983305c697531b1d65a88b1f6bc6eb6d5
Joel Blenman <blenmanjoel@gmail.com>
2025-03-19 18:17:59 -0400

    Refactor NavBar and RightPanel components for improved styling; update CSS variables for consistent border usage

--- front-end/src/components/NavBar.jsx
+++ front-end/src/components/NavBar.jsx
@@ -4,7 +4,7 @@ import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs"
 export function NavBar({ activeTab, setActiveTab }) {
   return (
     
-    <div className="border-b px-10">
+    <div className="border-b border-b-border px-10">
     <div className="mx-auto px-4 flex items-center justify-between py-2"> 
       <h1 className="text-xl font-bold">Graph Visualization</h1>
       <TabsList className="ml-auto">
